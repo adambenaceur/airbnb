c239965653f8b98a78b1914c52c6b174a94c287d
adambenaceur <benaceur.adam@gmail.com>
2022-03-11 15:05:59 -0700

    added react-map-gl and mapbox integration

---
+++ components/Map.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import getCenter from "geolib/es/getCenter"
+
+function Map({searchResults}) {
+  
+
+  
+    // Transform the search results object into the {latitude: x , longitude: y  } object 
+
+    const coordinates = searchResults.map(result => ({
+        longitude: result.long,
+        latitude: result.lat,
+    }))
+
+    // The latitude and longitude of the center of locations coordinates
+    const center  = getCenter(coordinates);
+
+    const [viewport, setViewport] = useState({
+        width: '100%',
+        height: '100%',
+        longitude: center.longitude,
+        latitude: center.latitude,
+        zoom: 11,
+    });
+
+
+    return (
+    <ReactMapGL
+        mapStyle='mapbox://styles/adambenaceur/cl0lzgh2d000914qetlkzk8ky'
+        mapboxAccessToken={process.env.mapbox_key}
+        
+        {...viewport}//spread operator
+        
+        // onViewportChange={(viewport) => setViewport(viewport)}
+        onMove={evt => setViewport(evt.viewport)}
+    >
+      
+    </ReactMapGL>
+  );
+}
+
+export default Map
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    mapbox_key: 'pk.eyJ1IjoiYWRhbWJlbmFjZXVyIiwiYSI6ImNsMGx5ZG1nejEwdXkza3I1N3cybmcyeHcifQ.EHfQHICbwQwxzPLuaVZlQg'
+  }
 }
